07e18d6e143b911bb0a266ae198eb31121d87e90
Kurt Jensen <Kurtmj93@gmail.com>
2023-04-06 20:45:38 -0400

    write and connect friend routes to user

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,7 +34,7 @@ const userController = {
                 res.status(500).json(err);
             });
     },
-    deleteUser(req, res) {
+    deleteUser(req, res) { // TODO, BONUS: also delete user's associated thoughts
         User.findOneAndRemove({ _id: req.params.userId })
         .then((userData) => {
             if (!userData) {
@@ -61,6 +61,38 @@ const userController = {
             } else { 
                 res.json(userData);
             } 
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    addFriend(req, res) {
+        User.findOneAndUpdate({ _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true })
+        .then((userData) => {
+            if (!userData) {
+                return res.status(404).json({message: 'No user found with that ID'});
+            } else {
+                res.json(userData);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    unFriend(req, res) {
+        User.findOneAndUpdate({ _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } }
+            )
+        .then((userData) => {
+            if (!userData) {
+                return res.status(404).json({message: 'No user found with that ID'});
+            } else {
+                res.json(userData); // BUG: userData still shows friendId in this response, but it is deleted
+            }
         })
         .catch((err) => {
             console.log(err);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const { getUsers, getOneUser, createUser, deleteUser, updateUser } = require('../../controllers/userController');
+const { getUsers, getOneUser, createUser, deleteUser, updateUser, addFriend, unFriend } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getOneUser).delete(deleteUser).put(updateUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(unFriend);
 
 module.exports = router;
